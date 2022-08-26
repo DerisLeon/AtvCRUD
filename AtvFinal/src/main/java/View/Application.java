051afd474e740c3c15eea7c5684a730ca0f819d3
Déris Araújo <79857414+DerisLeon@users.noreply.github.com>
2022-08-26 14:47:08 -0300

    Add files via upload

--- AtvFinal/src/main/java/View/Application.java
+++ AtvFinal/src/main/java/View/Application.java
@@ -15,45 +15,54 @@ public class Application {
                 long tempoInicial = System.currentTimeMillis();
             	//INSERT
 		Pessoa pessoa = new Pessoa(null, "44444444444", "Barros Sousa", "22/12/1973", "HTML, CSS, JAVA, Postgres");
-             
-		entityManager.getTransaction().begin();
-		entityManager.persist(pessoa);
-		entityManager.getTransaction().commit();
+                
+                
+                for(int i = 0; i < 1000;i++){
+                    entityManager.getTransaction().begin();
+                    entityManager.persist(pessoa);
+                    entityManager.getTransaction().commit();
+                    pessoa = new Pessoa(null, "44444444444", "Barros Sousa", "22/12/1973", "HTML, CSS, JAVA, Postgres");
+                }
+                
+                        System.out.println("Completou");
+                    
+
+                System.out.println(pessoa);
                  
 
                 // FIND
-		//Pessoa pessoa = entityManager.find(Pessoa.class, 1);
-		//System.out.println("Nome do cliente: " + pessoa.getNome());
-                //System.out.println("Data de Nascimento: " + pessoa.getNascimento());
-                //System.out.println("Formação: " + pessoa.getFormacao());
+		Pessoa pessoa3 = entityManager.find(Pessoa.class, 98);
+		System.out.println("Nome do cliente: " + pessoa3.getNome());
+                System.out.println("Data de Nascimento: " + pessoa3.getNascimento());
+                System.out.println("Formação: " + pessoa3.getFormacao());
 		
 
    
 		
 		// DELETE
 		
-//              Pessoa pessoa = entityManager.find(Pessoa.class, 2);
-//		entityManager.getTransaction().begin();
-//		entityManager.remove(pessoa);
-//		entityManager.getTransaction().commit();
+              Pessoa pessoa2 = entityManager.find(Pessoa.class, 103);
+		entityManager.getTransaction().begin();
+		entityManager.remove(pessoa2);
+		entityManager.getTransaction().commit();
                 
 		
                 
 		//UPDATE
                 
-//		Pessoa pessoa = new Pessoa();
-//		pessoa.setId(2);
-//		pessoa.setNome("Jose da Silva");
-//              pessoa.setNascimento("11/11/1970");
-//              pessoa.setCpf("11111111111");
-//              pessoa.setFormacao("HTML, CSS, JAVA, Postgres");
-//		
-//		entityManager.getTransaction().begin();
-//		entityManager.merge(pessoa);
-//		entityManager.getTransaction().commit();
-//
-//		entityManager.close();
-//		entityManagerFactory.close();
+		Pessoa pessoa1 = new Pessoa();
+		pessoa.setId(2);
+		pessoa.setNome("Jose da Silva");
+                pessoa.setNascimento("11/11/1970");
+                pessoa.setCpf("11111111111");
+                pessoa.setFormacao("HTML, CSS, JAVA, Postgres");
+		
+		entityManager.getTransaction().begin();
+		entityManager.merge(pessoa1);
+		entityManager.getTransaction().commit();
+
+		entityManager.close();
+		entityManagerFactory.close();
 
 
                 long tempoFinal = System.currentTimeMillis();
